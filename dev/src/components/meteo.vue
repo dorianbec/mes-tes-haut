69c4714466a04305fb32439628b0791dd373f3ab
MonaMarchetti <marchetti.mona@gmail.com>
2019-04-03 09:56:21 +0200

    test API 2

--- dev/src/components/meteo.vue
+++ dev/src/components/meteo.vue
@@ -1,25 +1,30 @@
 <template>
-    <header class="header">
-      {{ info }}
-    </header>
+  <div id="app">
+    <h1>Bitcoin Price Index</h1>
+    <div v-for="currency in info" class="currency">
+      {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
-import json from '/home/user/movie-list/src/movie.json'
-import axios from 'axios'
+  import axios from 'axios'
 
-export default {
-  data () {
-    return {
-      info: null
+  export default {
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response.data.bpi))
     }
-  },
-  mounted () {
-    axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
   }
-}
 </script>
 
 <style scoped>
